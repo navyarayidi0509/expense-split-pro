079e9993b7dc6bdb504ed4cfc1a04962f761594b
navya <rayidinavya@gmail.com>
2026-02-21 13:36:37 -0500

    fix: add missing newlines

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,35 @@
-import js from '@eslint/js';
 import prettierConfig from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
-  js.configs.recommended,
-  prettierConfig,
   {
+    files: ['**/*.js', '**/*.jsx'],
+    languageOptions: {
+      parser: (await import('@babel/eslint-parser')).default,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        require: 'readonly',
+        module: 'readonly',
+        __dirname: 'readonly',
+      },
+    },
     plugins: {
       prettier: prettierPlugin,
     },
     rules: {
+      ...prettierConfig.rules,
       'prettier/prettier': 'error',
-      'no-unused-vars': 'warn',
+      'no-unused-vars': 'off',
       'no-console': 'off',
+      'no-undef': 'error',
     },
-    ignores: [
-      'node_modules/',
-      '.expo/',
-      'dist/',
-      'build/',
-    ],
+    ignores: ['node_modules/', '.expo/', 'dist/', 'build/'],
   },
 ];
